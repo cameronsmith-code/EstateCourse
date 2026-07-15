f302232bb2844b7d84fa8746d377091285bdc309
cameronsmith-code <cameron.smith@ipcsecurities.com>
2026-07-15 18:15:34 -0400

    Updated StepForm.tsx

--- src/components/StepForm.tsx
+++ src/components/StepForm.tsx
@@ -10846,6 +10846,9 @@ export default function StepForm({
                   </>
                 )}
 
+                {/* Additional Properties subheading */}
+                <h4 className="text-base font-semibold text-blue-400 mt-6 mb-1">Additional Properties</h4>
+
                 {/* Ownership gate questions */}
                 {ownershipGateQuestions.map(renderQuestion)}
 
